637ed0b5192338dbe514cc1f09b2b4132a871057
Abraham <abraham.22mx@gmail.com>
2026-03-27 15:28:45 -0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -69,11 +69,28 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+# Buscar la AMI más reciente de Ubuntu
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 # Creacion de la maquina virtual
 
 resource "aws_instance" "ubuntu_ec2" {
-  ami           = "ami-0fc5d935ebf8bc3bc"  
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id  
+  instance_type = "t3.micro"
   key_name      = aws_key_pair.aws_key.key_name
   security_groups = [aws_security_group.ec2_sg.name]
 
